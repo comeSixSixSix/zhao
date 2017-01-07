5c6eceb7e710642de682e7b8a2f34af4a5d32f5e
comeSixSixSix <13613302851@163.com>
2017-01-07 12:48:13 +0800

    Revert "123412345"
    This reverts commit c9c47e99a7b3a678e495381d9de4f08126593f08.

--- new/new/ViewController.m
+++ new/new/ViewController.m
@@ -17,12 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    UIButton *  btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn.frame = CGRectMake(100, 100, 100, 30);
-    [btn setTitle:@"按钮" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal] ;
-    [btn addTarget:self action:@selector(clickOne) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
 }
 
 
